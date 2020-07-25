1b7376cb5f37053ff2af5919487da856adffae5f
SBastos800 <soniavsbastos@gmail.com>
2020-07-25 17:30:00 +0100

    organised code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const addProduct = document.querySelector('.addProduct');
 const list = document.querySelector('.list');
 const searchForm = document.querySelector('.searchForm input');
 
-
+//add a new item to the list
 addProduct.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -11,22 +11,20 @@ addProduct.addEventListener('submit', e => {
     list.innerHTML += `
     <li>
         <span>${newProduct}</span>
-        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-trash-alt delete"></i>
     </li>`;
 
-    // scrollTo(0, 0);
-
     addProduct.reset();
-})
+});
 
-
+//delete and item from the list
 list.addEventListener('click', (e) => {
- if (e.target.classList.contains('delete')) {
-     e.target.parentElement.remove()
- }
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove()
+    }
+});
 
-
-})
+//filter items according to their class in order to show or hide
 const filteredItems = (itemTyped) => {
     Array.from(list.children)
         .filter((item) => {
@@ -43,12 +41,12 @@ const filteredItems = (itemTyped) => {
         .forEach((item) => {
             item.classList.remove("filtered");
         });
-}
+};
 
 searchForm.addEventListener('keyup', () => {
     const itemTyped = searchForm.value.toLowerCase().trim();
     filteredItems(itemTyped);
-})
+});
 
 
 
